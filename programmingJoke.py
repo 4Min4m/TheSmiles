fbbba17be59aca9ce41b74662511588d47aa6b25
Amin <36745993+4Min4m@users.noreply.github.com>
2025-02-22 10:30:46 +0100

    Create programmingJoke.py

---
+++ programmingJoke.py
@@ -0,0 +1,10 @@
+# first you need install pyjokes
+# pip install pyjokes
+
+import pyjokes
+
+# Generate a random programming joke
+
+joke = pyjokes.get_joke()
+
+print(f"Here's a programming joke for you: {joke}")
